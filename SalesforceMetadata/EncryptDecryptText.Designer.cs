d126dd3f0ed80caa56c7d0ea967440074738517d
Marcus Harikian <marcus.harikian@gmail.com>
2022-01-26 08:32:13 -0800

    Encrypt Decrypt Text text box anchor

--- SalesforceMetadata/EncryptDecryptText.Designer.cs
+++ SalesforceMetadata/EncryptDecryptText.Designer.cs
@@ -43,38 +43,36 @@
             // 
             // tbSharedSecret
             // 
-            this.tbSharedSecret.Location = new System.Drawing.Point(149, 9);
-            this.tbSharedSecret.Margin = new System.Windows.Forms.Padding(4);
+            this.tbSharedSecret.Location = new System.Drawing.Point(112, 7);
             this.tbSharedSecret.Name = "tbSharedSecret";
-            this.tbSharedSecret.Size = new System.Drawing.Size(584, 22);
+            this.tbSharedSecret.Size = new System.Drawing.Size(439, 20);
             this.tbSharedSecret.TabIndex = 1;
             // 
             // tbSalt
             // 
-            this.tbSalt.Location = new System.Drawing.Point(149, 41);
-            this.tbSalt.Margin = new System.Windows.Forms.Padding(4);
+            this.tbSalt.Location = new System.Drawing.Point(112, 33);
             this.tbSalt.Name = "tbSalt";
-            this.tbSalt.Size = new System.Drawing.Size(584, 22);
+            this.tbSalt.Size = new System.Drawing.Size(439, 20);
             this.tbSalt.TabIndex = 3;
             // 
             // tbEncryptedText
             // 
             this.tbEncryptedText.AcceptsReturn = true;
-            this.tbEncryptedText.Location = new System.Drawing.Point(149, 156);
-            this.tbEncryptedText.Margin = new System.Windows.Forms.Padding(4);
+            this.tbEncryptedText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbEncryptedText.Location = new System.Drawing.Point(112, 127);
             this.tbEncryptedText.Multiline = true;
             this.tbEncryptedText.Name = "tbEncryptedText";
             this.tbEncryptedText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tbEncryptedText.Size = new System.Drawing.Size(1052, 296);
+            this.tbEncryptedText.Size = new System.Drawing.Size(790, 241);
             this.tbEncryptedText.TabIndex = 5;
             this.tbEncryptedText.WordWrap = false;
             // 
             // btnEncryptDecrypt
             // 
-            this.btnEncryptDecrypt.Location = new System.Drawing.Point(385, 95);
-            this.btnEncryptDecrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEncryptDecrypt.Location = new System.Drawing.Point(289, 77);
             this.btnEncryptDecrypt.Name = "btnEncryptDecrypt";
-            this.btnEncryptDecrypt.Size = new System.Drawing.Size(199, 28);
+            this.btnEncryptDecrypt.Size = new System.Drawing.Size(149, 23);
             this.btnEncryptDecrypt.TabIndex = 8;
             this.btnEncryptDecrypt.Text = "Encrypt Text";
             this.btnEncryptDecrypt.UseVisualStyleBackColor = true;
@@ -83,60 +81,61 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 9);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(12, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "Shared Secret";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 41);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(12, 33);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Salt";
             // 
             // lblEncryptedText
             // 
+            this.lblEncryptedText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblEncryptedText.AutoSize = true;
-            this.lblEncryptedText.Location = new System.Drawing.Point(12, 159);
-            this.lblEncryptedText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEncryptedText.Location = new System.Drawing.Point(9, 129);
             this.lblEncryptedText.Name = "lblEncryptedText";
-            this.lblEncryptedText.Size = new System.Drawing.Size(103, 17);
+            this.lblEncryptedText.Size = new System.Drawing.Size(79, 13);
             this.lblEncryptedText.TabIndex = 4;
             this.lblEncryptedText.Text = "Encrypted Text";
             // 
             // tbDecryptedText
             // 
-            this.tbDecryptedText.Location = new System.Drawing.Point(149, 480);
-            this.tbDecryptedText.Margin = new System.Windows.Forms.Padding(4);
+            this.tbDecryptedText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDecryptedText.Location = new System.Drawing.Point(112, 390);
             this.tbDecryptedText.Multiline = true;
             this.tbDecryptedText.Name = "tbDecryptedText";
             this.tbDecryptedText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.tbDecryptedText.Size = new System.Drawing.Size(1052, 412);
+            this.tbDecryptedText.Size = new System.Drawing.Size(790, 336);
             this.tbDecryptedText.TabIndex = 10;
             this.tbDecryptedText.TextChanged += new System.EventHandler(this.tbDecryptedText_TextChanged);
             // 
             // lblDecryptedText
             // 
+            this.lblDecryptedText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblDecryptedText.AutoSize = true;
-            this.lblDecryptedText.Location = new System.Drawing.Point(11, 480);
-            this.lblDecryptedText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDecryptedText.Location = new System.Drawing.Point(8, 390);
             this.lblDecryptedText.Name = "lblDecryptedText";
-            this.lblDecryptedText.Size = new System.Drawing.Size(104, 17);
+            this.lblDecryptedText.Size = new System.Drawing.Size(80, 13);
             this.lblDecryptedText.TabIndex = 9;
             this.lblDecryptedText.Text = "Decrypted Text";
             // 
             // btnSaveEncryptedText
             // 
-            this.btnSaveEncryptedText.Location = new System.Drawing.Point(617, 95);
-            this.btnSaveEncryptedText.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSaveEncryptedText.Location = new System.Drawing.Point(463, 77);
             this.btnSaveEncryptedText.Name = "btnSaveEncryptedText";
-            this.btnSaveEncryptedText.Size = new System.Drawing.Size(199, 28);
+            this.btnSaveEncryptedText.Size = new System.Drawing.Size(149, 23);
             this.btnSaveEncryptedText.TabIndex = 11;
             this.btnSaveEncryptedText.Text = "Save Encrypted To File";
             this.btnSaveEncryptedText.UseVisualStyleBackColor = true;
@@ -144,10 +143,9 @@
             // 
             // btnDecryptText
             // 
-            this.btnDecryptText.Location = new System.Drawing.Point(149, 95);
-            this.btnDecryptText.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDecryptText.Location = new System.Drawing.Point(112, 77);
             this.btnDecryptText.Name = "btnDecryptText";
-            this.btnDecryptText.Size = new System.Drawing.Size(199, 28);
+            this.btnDecryptText.Size = new System.Drawing.Size(149, 23);
             this.btnDecryptText.TabIndex = 12;
             this.btnDecryptText.Text = "Decrypt Text";
             this.btnDecryptText.UseVisualStyleBackColor = true;
@@ -155,9 +153,9 @@
             // 
             // EncryptDecryptText
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1224, 905);
+            this.ClientSize = new System.Drawing.Size(918, 735);
             this.Controls.Add(this.btnDecryptText);
             this.Controls.Add(this.btnSaveEncryptedText);
             this.Controls.Add(this.lblDecryptedText);
@@ -169,7 +167,6 @@
             this.Controls.Add(this.tbEncryptedText);
             this.Controls.Add(this.tbSalt);
             this.Controls.Add(this.tbSharedSecret);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "EncryptDecryptText";
             this.Text = "Encrypt / Decrypt Text";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.EncryptDecryptText_FormClosing);

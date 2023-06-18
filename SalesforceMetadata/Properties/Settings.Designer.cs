45d30eaacb83f66e9b08e3bad3c2aca212ebc842
Marcus Harikian <marcus.harikian@gmail.com>
2023-06-17 18:09:26 -0700

    Updated Default API Version

--- SalesforceMetadata/Properties/Settings.Designer.cs
+++ SalesforceMetadata/Properties/Settings.Designer.cs
@@ -91,7 +91,7 @@ namespace SalesforceMetadata.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("56.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("58.0")]
         public string DefaultAPI {
             get {
                 return ((string)(this["DefaultAPI"]));

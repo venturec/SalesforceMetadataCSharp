ca9db08cca9ec54b9a33ed06c40ebaf8c80dec69
Marcus Harikian <marcus.harikian@gmail.com>
2025-07-21 08:51:28 -0700

    Updates to SobjectFieldInspector

--- SalesforceMetadata/ObjectFieldInspector.Designer.cs
+++ SalesforceMetadata/ObjectFieldInspector.Designer.cs
@@ -41,25 +41,27 @@
             this.cmbUserName = new System.Windows.Forms.ComboBox();
             this.btnSaveSelectedToExcel = new System.Windows.Forms.Button();
             this.sobjectListBox = new System.Windows.Forms.CheckedListBox();
-            this.grpSaveToExcel = new System.Windows.Forms.GroupBox();
+            this.saveToExcelGrpBox = new System.Windows.Forms.GroupBox();
             this.cbSelectNone = new System.Windows.Forms.CheckBox();
             this.cbSelectAll = new System.Windows.Forms.CheckBox();
             this.btnGetReferenceFields = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.salesforceSobjectsGrpBox = new System.Windows.Forms.GroupBox();
             this.btnExportSelected = new System.Windows.Forms.Button();
             this.cbFilterManagedPkg = new System.Windows.Forms.CheckBox();
             this.cbCustomObjectsOnly = new System.Windows.Forms.CheckBox();
             this.salesforceOrgCredentials.SuspendLayout();
-            this.grpSaveToExcel.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.saveToExcelGrpBox.SuspendLayout();
+            this.salesforceSobjectsGrpBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // cmbSalesforceSObjects
             // 
+            this.cmbSalesforceSObjects.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cmbSalesforceSObjects.FormattingEnabled = true;
             this.cmbSalesforceSObjects.Location = new System.Drawing.Point(10, 18);
             this.cmbSalesforceSObjects.Name = "cmbSalesforceSObjects";
-            this.cmbSalesforceSObjects.Size = new System.Drawing.Size(426, 21);
+            this.cmbSalesforceSObjects.Size = new System.Drawing.Size(585, 24);
             this.cmbSalesforceSObjects.TabIndex = 1;
             this.cmbSalesforceSObjects.SelectedValueChanged += new System.EventHandler(this.cmbSalesforceSObjects_SelectedValueChanged);
             // 
@@ -68,7 +70,7 @@
             this.lblSalesforceSobjects.AutoSize = true;
             this.lblSalesforceSobjects.Location = new System.Drawing.Point(8, 0);
             this.lblSalesforceSobjects.Name = "lblSalesforceSobjects";
-            this.lblSalesforceSobjects.Size = new System.Drawing.Size(101, 13);
+            this.lblSalesforceSobjects.Size = new System.Drawing.Size(133, 17);
             this.lblSalesforceSobjects.TabIndex = 0;
             this.lblSalesforceSobjects.Text = "Salesforce Sobjects";
             // 
@@ -102,7 +104,7 @@
             this.listViewSobjectFields.LabelEdit = true;
             this.listViewSobjectFields.Location = new System.Drawing.Point(16, 401);
             this.listViewSobjectFields.Name = "listViewSobjectFields";
-            this.listViewSobjectFields.Size = new System.Drawing.Size(1055, 408);
+            this.listViewSobjectFields.Size = new System.Drawing.Size(1222, 408);
             this.listViewSobjectFields.TabIndex = 4;
             this.listViewSobjectFields.UseCompatibleStateImageBehavior = false;
             this.listViewSobjectFields.View = System.Windows.Forms.View.Details;
@@ -114,7 +116,7 @@
             // 
             this.btnSaveFieldsToFile.Location = new System.Drawing.Point(11, 112);
             this.btnSaveFieldsToFile.Name = "btnSaveFieldsToFile";
-            this.btnSaveFieldsToFile.Size = new System.Drawing.Size(138, 25);
+            this.btnSaveFieldsToFile.Size = new System.Drawing.Size(138, 32);
             this.btnSaveFieldsToFile.TabIndex = 4;
             this.btnSaveFieldsToFile.Text = "Save Fields To File";
             this.btnSaveFieldsToFile.UseVisualStyleBackColor = true;
@@ -127,6 +129,7 @@
             this.salesforceOrgCredentials.Controls.Add(this.btnSaveObjectsToFile);
             this.salesforceOrgCredentials.Controls.Add(this.cmbUserName);
             this.salesforceOrgCredentials.Controls.Add(this.btnGetSobjects);
+            this.salesforceOrgCredentials.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.salesforceOrgCredentials.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.salesforceOrgCredentials.Location = new System.Drawing.Point(9, 17);
             this.salesforceOrgCredentials.Margin = new System.Windows.Forms.Padding(2);
@@ -173,10 +176,10 @@
             // btnSaveSelectedToExcel
             // 
             this.btnSaveSelectedToExcel.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.btnSaveSelectedToExcel.Location = new System.Drawing.Point(462, 112);
+            this.btnSaveSelectedToExcel.Location = new System.Drawing.Point(245, 17);
             this.btnSaveSelectedToExcel.Margin = new System.Windows.Forms.Padding(2);
             this.btnSaveSelectedToExcel.Name = "btnSaveSelectedToExcel";
-            this.btnSaveSelectedToExcel.Size = new System.Drawing.Size(137, 25);
+            this.btnSaveSelectedToExcel.Size = new System.Drawing.Size(196, 31);
             this.btnSaveSelectedToExcel.TabIndex = 3;
             this.btnSaveSelectedToExcel.Text = "Save Selected to Excel";
             this.btnSaveSelectedToExcel.UseVisualStyleBackColor = false;
@@ -184,38 +187,43 @@
             // 
             // sobjectListBox
             // 
-            this.sobjectListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sobjectListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.sobjectListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.sobjectListBox.FormattingEnabled = true;
-            this.sobjectListBox.Location = new System.Drawing.Point(15, 17);
+            this.sobjectListBox.Location = new System.Drawing.Point(15, 89);
             this.sobjectListBox.Margin = new System.Windows.Forms.Padding(2);
             this.sobjectListBox.Name = "sobjectListBox";
-            this.sobjectListBox.Size = new System.Drawing.Size(426, 184);
+            this.sobjectListBox.Size = new System.Drawing.Size(426, 697);
             this.sobjectListBox.TabIndex = 0;
             // 
-            // grpSaveToExcel
+            // saveToExcelGrpBox
             // 
-            this.grpSaveToExcel.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.grpSaveToExcel.Controls.Add(this.cbSelectNone);
-            this.grpSaveToExcel.Controls.Add(this.cbSelectAll);
-            this.grpSaveToExcel.Controls.Add(this.btnGetReferenceFields);
-            this.grpSaveToExcel.Controls.Add(this.sobjectListBox);
-            this.grpSaveToExcel.Controls.Add(this.btnSaveSelectedToExcel);
-            this.grpSaveToExcel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.grpSaveToExcel.Location = new System.Drawing.Point(460, 156);
-            this.grpSaveToExcel.Margin = new System.Windows.Forms.Padding(2);
-            this.grpSaveToExcel.Name = "grpSaveToExcel";
-            this.grpSaveToExcel.Padding = new System.Windows.Forms.Padding(2);
-            this.grpSaveToExcel.Size = new System.Drawing.Size(614, 223);
-            this.grpSaveToExcel.TabIndex = 2;
-            this.grpSaveToExcel.TabStop = false;
-            this.grpSaveToExcel.Text = "Save Selected To Excel";
+            this.saveToExcelGrpBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saveToExcelGrpBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.saveToExcelGrpBox.Controls.Add(this.cbSelectNone);
+            this.saveToExcelGrpBox.Controls.Add(this.cbSelectAll);
+            this.saveToExcelGrpBox.Controls.Add(this.btnGetReferenceFields);
+            this.saveToExcelGrpBox.Controls.Add(this.sobjectListBox);
+            this.saveToExcelGrpBox.Controls.Add(this.btnSaveSelectedToExcel);
+            this.saveToExcelGrpBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveToExcelGrpBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveToExcelGrpBox.Location = new System.Drawing.Point(1243, 17);
+            this.saveToExcelGrpBox.Margin = new System.Windows.Forms.Padding(2);
+            this.saveToExcelGrpBox.Name = "saveToExcelGrpBox";
+            this.saveToExcelGrpBox.Padding = new System.Windows.Forms.Padding(2);
+            this.saveToExcelGrpBox.Size = new System.Drawing.Size(458, 793);
+            this.saveToExcelGrpBox.TabIndex = 2;
+            this.saveToExcelGrpBox.TabStop = false;
+            this.saveToExcelGrpBox.Text = "Save Selected To Excel";
             // 
             // cbSelectNone
             // 
             this.cbSelectNone.AutoSize = true;
-            this.cbSelectNone.Location = new System.Drawing.Point(462, 43);
+            this.cbSelectNone.Location = new System.Drawing.Point(15, 50);
             this.cbSelectNone.Name = "cbSelectNone";
-            this.cbSelectNone.Size = new System.Drawing.Size(85, 17);
+            this.cbSelectNone.Size = new System.Drawing.Size(104, 21);
             this.cbSelectNone.TabIndex = 2;
             this.cbSelectNone.Text = "Select None";
             this.cbSelectNone.UseVisualStyleBackColor = true;
@@ -224,9 +232,9 @@
             // cbSelectAll
             // 
             this.cbSelectAll.AutoSize = true;
-            this.cbSelectAll.Location = new System.Drawing.Point(462, 20);
+            this.cbSelectAll.Location = new System.Drawing.Point(15, 27);
             this.cbSelectAll.Name = "cbSelectAll";
-            this.cbSelectAll.Size = new System.Drawing.Size(70, 17);
+            this.cbSelectAll.Size = new System.Drawing.Size(85, 21);
             this.cbSelectAll.TabIndex = 1;
             this.cbSelectAll.Text = "Select All";
             this.cbSelectAll.UseVisualStyleBackColor = true;
@@ -234,39 +242,40 @@
             // 
             // btnGetReferenceFields
             // 
-            this.btnGetReferenceFields.Location = new System.Drawing.Point(462, 142);
+            this.btnGetReferenceFields.Location = new System.Drawing.Point(245, 50);
             this.btnGetReferenceFields.Name = "btnGetReferenceFields";
-            this.btnGetReferenceFields.Size = new System.Drawing.Size(137, 23);
+            this.btnGetReferenceFields.Size = new System.Drawing.Size(196, 34);
             this.btnGetReferenceFields.TabIndex = 4;
             this.btnGetReferenceFields.Text = "Get Reference Fields Only";
             this.btnGetReferenceFields.UseVisualStyleBackColor = true;
             this.btnGetReferenceFields.Click += new System.EventHandler(this.btnGetReferenceFields_Click);
             // 
-            // groupBox1
+            // salesforceSobjectsGrpBox
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.groupBox1.Controls.Add(this.btnExportSelected);
-            this.groupBox1.Controls.Add(this.cbFilterManagedPkg);
-            this.groupBox1.Controls.Add(this.cbCustomObjectsOnly);
-            this.groupBox1.Controls.Add(this.btnSaveFieldsToFile);
-            this.groupBox1.Controls.Add(this.lblSalesforceSobjects);
-            this.groupBox1.Controls.Add(this.cmbSalesforceSObjects);
-            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.groupBox1.Location = new System.Drawing.Point(9, 156);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(446, 223);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.salesforceSobjectsGrpBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.salesforceSobjectsGrpBox.Controls.Add(this.btnExportSelected);
+            this.salesforceSobjectsGrpBox.Controls.Add(this.cbFilterManagedPkg);
+            this.salesforceSobjectsGrpBox.Controls.Add(this.cbCustomObjectsOnly);
+            this.salesforceSobjectsGrpBox.Controls.Add(this.btnSaveFieldsToFile);
+            this.salesforceSobjectsGrpBox.Controls.Add(this.lblSalesforceSobjects);
+            this.salesforceSobjectsGrpBox.Controls.Add(this.cmbSalesforceSObjects);
+            this.salesforceSobjectsGrpBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salesforceSobjectsGrpBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.salesforceSobjectsGrpBox.Location = new System.Drawing.Point(9, 156);
+            this.salesforceSobjectsGrpBox.Margin = new System.Windows.Forms.Padding(2);
+            this.salesforceSobjectsGrpBox.Name = "salesforceSobjectsGrpBox";
+            this.salesforceSobjectsGrpBox.Padding = new System.Windows.Forms.Padding(2);
+            this.salesforceSobjectsGrpBox.Size = new System.Drawing.Size(601, 223);
+            this.salesforceSobjectsGrpBox.TabIndex = 1;
+            this.salesforceSobjectsGrpBox.TabStop = false;
+            this.salesforceSobjectsGrpBox.Text = "groupBox1";
             // 
             // btnExportSelected
             // 
             this.btnExportSelected.Enabled = false;
-            this.btnExportSelected.Location = new System.Drawing.Point(253, 114);
+            this.btnExportSelected.Location = new System.Drawing.Point(165, 112);
             this.btnExportSelected.Name = "btnExportSelected";
-            this.btnExportSelected.Size = new System.Drawing.Size(138, 23);
+            this.btnExportSelected.Size = new System.Drawing.Size(174, 32);
             this.btnExportSelected.TabIndex = 6;
             this.btnExportSelected.Text = "Export Selected to Excel";
             this.btnExportSelected.UseVisualStyleBackColor = true;
@@ -277,7 +286,7 @@
             this.cbFilterManagedPkg.AutoSize = true;
             this.cbFilterManagedPkg.Location = new System.Drawing.Point(10, 68);
             this.cbFilterManagedPkg.Name = "cbFilterManagedPkg";
-            this.cbFilterManagedPkg.Size = new System.Drawing.Size(147, 17);
+            this.cbFilterManagedPkg.Size = new System.Drawing.Size(187, 21);
             this.cbFilterManagedPkg.TabIndex = 5;
             this.cbFilterManagedPkg.Text = "Filter Managed Packages";
             this.cbFilterManagedPkg.UseVisualStyleBackColor = true;
@@ -288,7 +297,7 @@
             this.cbCustomObjectsOnly.AutoSize = true;
             this.cbCustomObjectsOnly.Location = new System.Drawing.Point(11, 45);
             this.cbCustomObjectsOnly.Name = "cbCustomObjectsOnly";
-            this.cbCustomObjectsOnly.Size = new System.Drawing.Size(125, 17);
+            this.cbCustomObjectsOnly.Size = new System.Drawing.Size(161, 21);
             this.cbCustomObjectsOnly.TabIndex = 2;
             this.cbCustomObjectsOnly.Text = "Filter Custom Objects";
             this.cbCustomObjectsOnly.UseVisualStyleBackColor = true;
@@ -297,9 +306,9 @@
             // ObjectFieldInspector
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(1114, 821);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.grpSaveToExcel);
+            this.ClientSize = new System.Drawing.Size(1712, 821);
+            this.Controls.Add(this.salesforceSobjectsGrpBox);
+            this.Controls.Add(this.saveToExcelGrpBox);
             this.Controls.Add(this.salesforceOrgCredentials);
             this.Controls.Add(this.listViewSobjectFields);
             this.Controls.Add(this.lblSobjectFieldList);
@@ -308,10 +317,10 @@
             this.Text = "ObjectFieldInspector";
             this.salesforceOrgCredentials.ResumeLayout(false);
             this.salesforceOrgCredentials.PerformLayout();
-            this.grpSaveToExcel.ResumeLayout(false);
-            this.grpSaveToExcel.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.saveToExcelGrpBox.ResumeLayout(false);
+            this.saveToExcelGrpBox.PerformLayout();
+            this.salesforceSobjectsGrpBox.ResumeLayout(false);
+            this.salesforceSobjectsGrpBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -330,8 +339,8 @@
         private System.Windows.Forms.Button btnSaveObjectsToFile;
         private System.Windows.Forms.Button btnSaveSelectedToExcel;
         private System.Windows.Forms.CheckedListBox sobjectListBox;
-        private System.Windows.Forms.GroupBox grpSaveToExcel;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox saveToExcelGrpBox;
+        private System.Windows.Forms.GroupBox salesforceSobjectsGrpBox;
         private System.Windows.Forms.Button btnGetReferenceFields;
         private System.Windows.Forms.CheckBox cbSelectNone;
         private System.Windows.Forms.CheckBox cbSelectAll;
